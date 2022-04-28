a3e9e5cc8a51f81b3fbd20143880529dfc57a80a
Mohammed abdulaziz <mohaziz101@hotmail.com>
2022-04-28 15:02:10 +0200

    Implementing changes according to the feedback 2

--- week2/project/ecommerce/src/fake-data/all-categories.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default [
-    "FAKE: electronics",
-    "FAKE: jewelery",
-    "FAKE: men's clothing",
-    "FAKE: women's clothing",
-];
